5a27687e56b10938b2f49a74603c6c6c9e12c649
Stein <gabriel.victor@morningstarint.com>
2024-10-10 00:28:02 -0300

    Refactored Base Configuration to Support Both CYW43 and FreeRTOS with Multithreading

--- config/lwipopts.h
+++ config/lwipopts.h
@@ -1,5 +1,5 @@
-#ifndef __LWIPOPTS_H__
-#define __LWIPOPTS_H__
+#ifndef _LWIPOPTS_H
+#define _LWIPOPTS_H
 
 // Common settings used in most of the pico_w examples
 // (see https://www.nongnu.org/lwip/2_1_x/group__lwip__opts.html for details)
@@ -85,5 +85,31 @@
 #define PPP_DEBUG                   LWIP_DBG_OFF
 #define SLIP_DEBUG                  LWIP_DBG_OFF
 #define DHCP_DEBUG                  LWIP_DBG_OFF
+
+// The following is needed to test mDns
+#define LWIP_MDNS_RESPONDER 1
+#define LWIP_IGMP 1
+#define LWIP_NUM_NETIF_CLIENT_DATA 1
+#define MDNS_RESP_USENETIF_EXTCALLBACK  1
+#define MEMP_NUM_SYS_TIMEOUT (LWIP_NUM_SYS_TIMEOUT_INTERNAL + 3)
+#define MEMP_NUM_TCP_PCB 12
+
+// Enable cgi and ssi
+#define LWIP_HTTPD_CGI 1
+#define LWIP_HTTPD_SSI 1
+#define LWIP_HTTPD_SSI_MULTIPART 1
+
+#if !NO_SYS
+#define TCPIP_THREAD_STACKSIZE 2048 // mDNS needs more stack
+#define DEFAULT_THREAD_STACKSIZE 1024
+#define DEFAULT_RAW_RECVMBOX_SIZE 8
+#define TCPIP_MBOX_SIZE 8
+#define LWIP_TIMEVAL_PRIVATE 0
+
+// not necessary, can be done either way
+#define LWIP_TCPIP_CORE_LOCKING_INPUT 1
+#endif
+
+#define HTTPD_FSDATA_FILE "pico_fsdata.inc"
 
 #endif /* __LWIPOPTS_H__ */
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------------------------------
 //  File        : main.c
-//  Description : Main Source File for the Controller
+//  Description : Main Source File for the Nishmah Controller
 //  Author      : Stein
 //
 //  Creation Date     : 2024-10-02T03:13:37-03:00
@@ -12,36 +12,7 @@
 //  Copyright(C) 2024 MorningStar Corporation International. All Rights Reserved.
 // --------------------------------------------------------------------------------------------------
 
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-
 int main()
 {
-    stdio_init_all();
-
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
-
-    // Enable wifi station
-    cyw43_arch_enable_sta_mode();
-
-    printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        return 1;
-    } else {
-        printf("Connected.\n");
-        // Read the ip address in a human readable way
-        uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
-        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
-    }
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+	return 0;
 }
